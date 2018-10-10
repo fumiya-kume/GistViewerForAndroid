a195b18ff926b9567f7a0040b1bf6ac7ca7681a9
Fumiya Kume <fumiya.kume@hotmail.com>
2018-10-10 20:43:40 +0900

    separate name space and databinding

--- app/src/main/java/com/example/kuxu/gistviewerforandroid/ui/MainActivity.kt
+++ app/src/main/java/com/example/kuxu/gistviewerforandroid/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.kuxu.gistviewerforandroid
+package com.example.kuxu.gistviewerforandroid.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kuxu.gistviewerforandroid.R
 
 class MainActivity : AppCompatActivity() {
 
