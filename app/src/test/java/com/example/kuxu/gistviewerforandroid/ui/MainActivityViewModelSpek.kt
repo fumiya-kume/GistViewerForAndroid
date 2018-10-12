57eb1334f37ef627694bb199eab412539d23e385
Fumiya Kume <fumiya.kume@hotmail.com>
2018-10-12 13:38:00 +0900

    fix unit test

--- app/src/test/java/com/example/kuxu/gistviewerforandroid/ui/MainActivityViewModelSpek.kt
+++ app/src/test/java/com/example/kuxu/gistviewerforandroid/ui/MainActivityViewModelSpek.kt
@@ -1,18 +1,18 @@
 package com.example.kuxu.gistviewerforandroid.ui
 
 import com.example.kuxu.gistviewerforandroid.service.ChromeCustomTabsService
+import com.example.prop.GithubProperties
 import org.junit.Test
 import org.koin.test.KoinTest
-import org.mockito.Mockito
 import org.mockito.Mockito.*
 
 
-internal class MainActivityViewModelSpek: KoinTest{
-
+internal class MainActivityViewModelSpek : KoinTest {
     @Test
-    fun 画面が開かれた時にGithubのログイン画面を開く(){
-        val chromeCustomTabsService  = mock(ChromeCustomTabsService::class.java)
-        val viewModel = MainActivityViewModel(chromeCustomTabsService)
+    fun 画面が開かれた時にGithubのログイン画面を開く() {
+        val chromeCustomTabsService = mock(ChromeCustomTabsService::class.java)
+        val githubProperties = mock(GithubProperties::class.java)
+        val viewModel = MainActivityViewModel(chromeCustomTabsService, githubProperties)
 
         viewModel.GithubLoginPage()
 
