c2cd0fbb437668f3dde6054383f70a081138a40b
Fumiya Kume <fumiya.kume@hotmail.com>
2018-10-14 19:59:51 +0900

    change scope

--- app/src/main/java/com/example/kuxu/gistviewerforandroid/ui/uiModule.kt
+++ app/src/main/java/com/example/kuxu/gistviewerforandroid/ui/uiModule.kt
@@ -3,7 +3,7 @@ package com.example.kuxu.gistviewerforandroid.ui
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
-val uiModule = module {
+internal val uiModule = module {
     viewModel {
         MainActivityViewModel(get(), get())
     }
