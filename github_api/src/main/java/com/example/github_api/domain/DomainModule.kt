204a4a99117802e0914f16890cf6bcc31ba79f52
Fumiya Kume <fumiya.kume@hotmail.com>
2018-10-14 20:06:06 +0900

    Github Service を適切なフォルダに移動した

--- github_api/src/main/java/com/example/github_api/domain/DomainModule.kt
+++ github_api/src/main/java/com/example/github_api/domain/DomainModule.kt
@@ -1,5 +1,6 @@
 package com.example.github_api.domain
 
+import com.example.github_api.domain.dependency.GithubService
 import com.example.github_api.infra.GithubServiceImpl
 import com.example.github_api.infra.domain.UserInfomationRepositoryImpl
 import org.koin.dsl.module.module
--- github_api/src/main/java/com/example/github_api/domain/dependency/GithubService.kt
+++ github_api/src/main/java/com/example/github_api/domain/dependency/GithubService.kt
@@ -1,7 +1,7 @@
-package com.example.github_api.domain
+package com.example.github_api.domain.dependency
 
 import kotlinx.coroutines.experimental.Deferred
 
-public interface GithubService {
+internal interface GithubService {
     fun loadGistCount():Deferred<Int>
 }
