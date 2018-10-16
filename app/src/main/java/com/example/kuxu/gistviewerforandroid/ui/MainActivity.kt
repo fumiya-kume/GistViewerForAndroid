5c82484d9933cc47b27a085e5143a77e70124447
Fumiya Kume <fumiya.kume@hotmail.com>
2018-10-16 19:17:52 +0900

    起動時のUX 改善

--- app/src/main/java/com/example/kuxu/gistviewerforandroid/ui/MainActivity.kt
+++ app/src/main/java/com/example/kuxu/gistviewerforandroid/ui/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.kuxu.gistviewerforandroid.ui
 
 import android.content.Intent
-import android.os.Bundle
+import android.net.Uri
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.github_api.Auth.GithubAuthcationService
@@ -9,6 +9,9 @@ import com.example.github_api.User.UserInfomationDataStore
 import com.example.github_api.sercret.AccessTokenRepository
 import com.example.kuxu.gistviewerforandroid.R
 import io.reactivex.rxkotlin.subscribeBy
+import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.GlobalScope
+import kotlinx.coroutines.experimental.launch
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -19,49 +22,58 @@ internal class MainActivity : AppCompatActivity() {
   val accessTokenRepository: AccessTokenRepository by inject()
   val userInfomationDataStore: UserInfomationDataStore by inject()
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+  override fun onResume() {
+    super.onResume()
     setContentView(R.layout.activity_main)
 
-    if (!accessTokenRepository.loadAccessToken().isEmpty()) {
-      userInfomationDataStore.userInfomation()
-        .subscribeBy(
-          onSuccess = {
-            ShowErrorMessage("Welcome to ${it.userName} !!")
-            navigateToHomeActivity()
-          },
-          onError = {
-            ShowErrorMessage("認証で問題が発生しました")
-          }
-        )
-    } else {
-      viewModel.GithubLoginPage()
-    }
+    GlobalScope.launch {
+      if (!accessTokenRepository.loadAccessToken().isEmpty()) {
+        userInfomationDataStore.userInfomation()
+          .subscribeBy(
+            onSuccess = {
+              ShowErrorMessage("Welcome to ${it.userName} !!")
+              navigateToHomeActivity()
+            },
+            onError = {
+              ShowErrorMessage("認証で問題が発生しました")
+            }
+          )
+      } else {
+        viewModel.GithubLoginPage()
+      }
 
-    val uri = intent?.data
-    uri?.apply {
-      if (uri.toString().startsWith("gist-viewer")) {
-        val queryResult = uri.getQueryParameter("code")
-        queryResult?.let {
-          githubAuthcationService.authWithCode(queryResult)
-            .flatMap { accessTokenRepository.saveAccessToken(it) }
-            .flatMap { userInfomationDataStore.userInfomation() }
-            .subscribeBy(
-              onSuccess = {
-                ShowErrorMessage("Welcome to ${it.userName}")
-                navigateToHomeActivity()
-              },
-              onError = {
-                ShowErrorMessage("AccessToken を取得に失敗しました")
-              }
-            )
+
+      val uri = intent?.data
+
+      fun checkOpenFromGithubAuthcation(uri: Uri) = uri.toString().startsWith("gist-viewer")
+      fun getAuthCode(Uri: Uri) = uri?.getQueryParameter("code")
+
+      uri?.apply {
+        if (checkOpenFromGithubAuthcation(uri)) {
+          getAuthCode(uri)?.let {
+            githubAuthcationService.authWithCode(it)
+              .flatMap { accessTokenRepository.saveAccessToken(it) }
+              .flatMap { userInfomationDataStore.userInfomation() }
+              .subscribeBy(
+                onSuccess = {
+                  ShowErrorMessage("Welcome to ${it.userName}")
+                  navigateToHomeActivity()
+                },
+                onError = {
+                  ShowErrorMessage("AccessToken を取得に失敗しました")
+                }
+              )
+          }
         }
       }
+
     }
   }
 
   private fun ShowErrorMessage(message: String) {
-    Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+    GlobalScope.launch(context = Dispatchers.Main) {
+      Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+    }
   }
 
   private fun navigateToHomeActivity() {
