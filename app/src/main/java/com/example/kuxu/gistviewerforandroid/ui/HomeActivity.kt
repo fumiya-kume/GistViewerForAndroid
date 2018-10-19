4cb94b256e7b805ddca06360e54ce34f414ee4d4
Fumiya Kume <fumiya.kume@hotmail.com>
2018-10-19 18:19:11 +0900

    ナビゲーションをいい感じにした

--- app/src/main/java/com/example/kuxu/gistviewerforandroid/ui/HomeActivity.kt
+++ app/src/main/java/com/example/kuxu/gistviewerforandroid/ui/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.example.kuxu.gistviewerforandroid.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.example.kuxu.gistviewerforandroid.R
 
 internal class HomeActivity : AppCompatActivity() {
@@ -9,4 +10,6 @@ internal class HomeActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_home)
   }
+
+  override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
 }
